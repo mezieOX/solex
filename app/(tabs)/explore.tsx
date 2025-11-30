17f86f7dbceb31275f79c576ac7ab65a83a02bc6
mezieOX <meziepage692@gmail.com>
2025-11-30 22:35:10 +0100

    first commit

---
+++ app/(tabs)/explore.tsx
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+import { ThemedText } from "@/components/themed-text";
+import { ThemedView } from "@/components/themed-view";
+
+export default function TabTwoScreen() {
+  return (
+    <ThemedView style={styles.container}>
+      <ThemedText>Explore Screen</ThemedText>
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
